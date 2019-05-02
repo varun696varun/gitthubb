2dfae9f12f47846b13e7689998faee264b8aef9c
Tony stark <varun696varun.vs@gmail.com>
2019-05-02 10:20:48 +0530

    third commit

---
+++ app/src/main/java/com/varun696/gitthubb/imagee.java
@@ -0,0 +1,13 @@
+package com.varun696.gitthubb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class imagee extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_imagee);
+    }
+}
